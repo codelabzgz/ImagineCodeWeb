ef2244dc694b15300a7c1f4de8525debb4b9898f
lAngelP <angelcanal97@gmail.com>
2018-04-30 18:05:09 +0200

    Add countdown

---
+++ assets/js/countdown.js
@@ -0,0 +1,32 @@
+// Set the date we're counting down to
+var countDownDate = new Date("Sep 28, 2018 20:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="countdown-content"
+  document.getElementById("countdown-content").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown-content").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+jQuery(function(){
+    jQuery('#countdown-content').flipcountdown();
+  })
