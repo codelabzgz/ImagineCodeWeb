1a660fd2b451e46e90d8eab3b3bffb3b47fe1d41
jgarciapueyo <jgarciapueyo@gmail.com>
2018-05-04 15:14:13 +0200

    Add countdown div

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,3 +29,23 @@ function writeMail(c) {
   val = $( "#" + c );
   val.attr('hre'+'f', 'mai'+'lto:'+a+'@'+b);
  }
+
+jQuery(function($){
+    //var NY = Math.round((new Date('1/01/2015 00:00:01')).getTime()/1000);
+    $('#countdown').flipcountdown({
+        size:'lg',
+        beforeDateTime:'9/28/2018 00:00:01'
+        /*tick:function(){
+            var nol = function(h){
+                return h>9?h:'0'+h;
+            }
+            var	range  	= NY-Math.round((new Date()).getTime()/1000),
+                secday = 86400, sechour = 3600,
+                days 	= parseInt(range/secday),
+                hours	= parseInt((range%secday)/sechour),
+                min		= parseInt(((range%secday)%sechour)/60),
+                sec		= ((range%secday)%sechour)%60;
+            return nol(days)+' '+nol(hours)+' '+nol(min)+' '+nol(sec);
+        }*/
+    });
+});
